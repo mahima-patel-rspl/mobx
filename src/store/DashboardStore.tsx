51128b3fdee6df25955aa93a21fb370b6d78a37c
mahima-patel-rspl <133002427+mahima-patel-rspl@users.noreply.github.com>
2023-05-11 11:31:04 +0530

    recently added data & most viewed data in Dashboard - Done

--- src/store/DashboardStore.tsx
+++ src/store/DashboardStore.tsx
@@ -1,20 +1,26 @@
 import { action, computed, makeObservable, observable } from "mobx";
 import { IRootStore } from "./RootStore";
 import { fetchFunction } from "../components/common/AxiosInstance";
+import {
+  MostViewedData,
+  RecentlyData,
+  TopCategoriesData,
+} from "../components/pages/InterfaceTypes";
 
-export interface IDashTopCategories {
-  avatar_url: string | null;
-  categoryId: number;
-  categoryName: string;
-  id: number;
-  name: string;
-  total_components: number;
-  total_views: string;
-  web_url: string;
-}
+//Dashboard - Top categories store
+// export interface IDashTopCategories {
+//   avatar_url: string | null;
+//   categoryId: number;
+//   categoryName: string;
+//   id: number;
+//   name: string;
+//   total_components: number;
+//   total_views: string;
+//   web_url: string;
+// }
 
 export class DashTopCategoriesStore {
-  categories: IDashTopCategories[] = [];
+  categories: TopCategoriesData[] = [];
   rootStore: IRootStore;
 
   constructor(rootStore: IRootStore) {
@@ -31,11 +37,63 @@ export class DashTopCategoriesStore {
       `api/techstack/top?limit=${limit}`
     );
     this.categories = getData.payload;
-    console.log("getData----->", getData.payload);
-    console.log("this.categories----->", this.categories);
+    // console.log("getData----->", getData.payload);
+    // console.log("this.categories----->", this.categories);
   }
 
   get getCategories() {
     return this.categories;
+  }
+}
+
+//Dashboard - Recently Added store
+export class RecentlyAddedStore {
+  recentlyAddedItems: RecentlyData[] = [];
+  rootStore: IRootStore;
+
+  constructor(rootStore: IRootStore) {
+    makeObservable(this, {
+      recentlyAddedItems: observable,
+      fetchAddedItems: action,
+      getAddedItems: computed,
+    });
+    this.rootStore = rootStore;
+  }
+
+  async fetchAddedItems() {
+    const getData: any = await fetchFunction("api/component/recent");
+    this.recentlyAddedItems = getData.payload;
+  }
+
+  get getAddedItems() {
+    return this.recentlyAddedItems;
+  }
+}
+
+//Dashboard - Most Viewed store
+export class MostViewedStore {
+  mostViewedData: MostViewedData[] = [];
+  rootstore: IRootStore;
+
+  constructor(rootStore: IRootStore) {
+    makeObservable(this, {
+      mostViewedData: observable,
+      fetchMostViewedData: action,
+      getMostViewedData: computed,
+    });
+
+    this.rootstore = rootStore;
+  }
+
+  async fetchMostViewedData(duration: any) {
+    const getData: any = await fetchFunction(
+      `api/dashboard/mostviewed?duration=${duration}`
+    );
+    this.mostViewedData = getData.payload;
+    console.log("getData----->", getData.payload);
+  }
+
+  get getMostViewedData() {
+    return this.mostViewedData;
   }
 }
--- src/store/RootStore.tsx
+++ src/store/RootStore.tsx
@@ -1,17 +1,27 @@
-import { DashTopCategoriesStore } from "./DashboardStore";
+import {
+  DashTopCategoriesStore,
+  MostViewedStore,
+  RecentlyAddedStore,
+} from "./DashboardStore";
 import { UserStore } from "./UserStore";
 
 export interface IRootStore {
   userStore: UserStore;
   dashTopCategoriesStore: DashTopCategoriesStore;
+  recentlyAddedStore: RecentlyAddedStore;
+  mostViewedStore: MostViewedStore;
 }
 
 export class RootStore implements IRootStore {
   userStore: UserStore;
   dashTopCategoriesStore: DashTopCategoriesStore;
+  recentlyAddedStore: RecentlyAddedStore;
+  mostViewedStore: MostViewedStore;
 
   constructor() {
     this.userStore = new UserStore(this);
     this.dashTopCategoriesStore = new DashTopCategoriesStore(this);
+    this.recentlyAddedStore = new RecentlyAddedStore(this);
+    this.mostViewedStore = new MostViewedStore(this);
   }
 }
