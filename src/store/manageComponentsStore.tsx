a4587f8975786c5428b6cdeb93318bfb7cdc1540
mahima-patel-rspl <133002427+mahima-patel-rspl@users.noreply.github.com>
2023-05-12 19:06:41 +0530

    Import api integrated

--- src/store/manageComponentsStore.tsx
+++ src/store/manageComponentsStore.tsx
@@ -10,13 +10,15 @@ import {
 } from "../components/common/AxiosInstance";
 
 export interface IAdminTagList {
-  feature_tags: {
-    id: number;
-    name: string;
-    createdAt: string;
-    type: string;
-    updatedAt: string;
-  };
+  feature_tags: [
+    {
+      id: number;
+      name: string;
+      createdAt: string;
+      type: string;
+      updatedAt: string;
+    }
+  ];
   frameworks: [
     {
       id: number;
@@ -26,13 +28,15 @@ export interface IAdminTagList {
       updatedAt: string;
     }
   ];
-  functionalTags: {
-    id: number;
-    name: string;
-    createdAt: string;
-    type: string;
-    updatedAt: string;
-  };
+  functionalTags: [
+    {
+      id: number;
+      name: string;
+      createdAt: string;
+      type: string;
+      updatedAt: string;
+    }
+  ];
   tags: [
     {
       id: number;
@@ -44,12 +48,49 @@ export interface IAdminTagList {
   ];
 }
 
+export interface IManageFilteredList {
+  author: [
+    {
+      id: number;
+      name: string;
+    }
+  ];
+  version: [
+    {
+      id: number;
+      name: string;
+    }
+  ];
+}
+
+export interface IAdminSyncTechstack {
+  count: number;
+  message: string;
+  status: boolean;
+}
+
+export interface IAdminSyncComponents {
+  count: number;
+  message: string;
+  status: boolean;
+}
+
+export interface IAdminSyncFiles {
+  count: number;
+  message: string;
+  status: boolean;
+}
+
 export class ManageComponentStore {
   rootStore: IRootStore;
 
   techStacks: Techstacks[] = [];
   adminTagList: IAdminTagList | undefined;
   manageComponentList: ManageComponents[] = [];
+  manageFilteredList: IManageFilteredList | undefined;
+  adminSyncTechstack: IAdminSyncTechstack | undefined;
+  adminSyncComponents: IAdminSyncComponents | undefined;
+  adminSyncFiles: IAdminSyncFiles | undefined;
 
   constructor(rootStore: IRootStore) {
     makeObservable(this, {
@@ -62,6 +103,18 @@ export class ManageComponentStore {
       manageComponentList: observable,
       fetchManageComponents: action,
       getManageComponents: computed,
+      manageFilteredList: observable,
+      fetchManageFilteredData: action,
+      getManageFilteredData: computed,
+      adminSyncTechstack: observable,
+      fetchAdminSyncTechstack: action,
+      getAdminSyncTechstack: computed,
+      adminSyncComponents: observable,
+      fetchAdminSyncComponents: action,
+      getAdminSyncComponents: computed,
+      adminSyncFiles: observable,
+      fetchAdminSyncFiles: action,
+      getAdminSyncFiles: computed,
     });
 
     this.rootStore = rootStore;
@@ -98,5 +151,45 @@ export class ManageComponentStore {
 
   get getManageComponents() {
     return this.manageComponentList;
+  }
+
+  //manage filtered data
+  async fetchManageFilteredData() {
+    const getData: any = await fetchFunction(`api/component/managefilterdata`);
+    this.manageFilteredList = getData.payload;
+  }
+
+  get getManageFilteredData() {
+    return this.manageFilteredList;
+  }
+
+  //admin sync techstack
+  async fetchAdminSyncTechstack() {
+    const getData: any = await fetchFunction(`api/gitlab/sync/techstacks`);
+    this.adminSyncTechstack = getData;
+  }
+
+  get getAdminSyncTechstack() {
+    return this.adminSyncTechstack;
+  }
+
+  //admin sync components
+  async fetchAdminSyncComponents() {
+    const getData: any = await fetchFunction(`api/gitlab/sync/components`);
+    this.adminSyncComponents = getData;
+  }
+
+  get getAdminSyncComponents() {
+    return this.adminSyncComponents;
+  }
+
+  //admin sync files
+  async fetchAdminSyncFiles() {
+    const getData: any = await fetchFunction(`api/gitlab/sync/files`);
+    this.adminSyncFiles = getData;
+  }
+
+  get getAdminSyncFiles() {
+    return this.adminSyncFiles;
   }
 }
